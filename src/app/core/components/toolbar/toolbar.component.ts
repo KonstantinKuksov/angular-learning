16f78bea7556a765b3f52abc40fdb251f8c7631a
KonstantinKuksov <kkuksov@gmail.com>
2021-08-17 09:12:11 +0300

    Feat
    Add counter into the Toolbar component

--- src/app/core/components/toolbar/toolbar.component.ts
+++ src/app/core/components/toolbar/toolbar.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../../auth/services/auth.service';
+import {Subject, Subscription} from "rxjs";
 
 @Component({
   selector: 'app-toolbar',
@@ -7,5 +8,22 @@ import { AuthService } from '../../../auth/services/auth.service';
   styleUrls: ['./toolbar.component.scss'],
 })
 export class ToolbarComponent {
-  constructor(public authService: AuthService) {}
+
+  counter: number = 0;
+  sub: Subscription;
+  count$: Subject<number> = new Subject<number>();
+
+  constructor(public authService: AuthService) {
+    this.sub = this.count$.subscribe();
+  }
+
+  increase() {
+    this.counter++;
+    this.count$.next(this.counter);
+  }
+
+  decrease() {
+    this.counter--;
+    this.count$.next(this.counter);
+  }
 }
